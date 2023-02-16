f36799653c00d4087f4d002cc405277d68e7db4a
ox7f <ox7f@protonmail.com>
2023-02-16 17:48:34 +0100

    feat: 🎸 searchbar component

---
+++ packages/frontend/src/components/UI/SearchBar.tsx
@@ -0,0 +1,24 @@
+import { ChangeEvent } from 'react';
+import { PrimitiveAtom, useAtomValue, useSetAtom } from 'jotai';
+import { NewAgent } from 'sf-girls-calculator-calculator';
+
+interface SearchBarI {
+  atom: PrimitiveAtom<NewAgent[]>;
+  sourceAtom: PrimitiveAtom<NewAgent[]>;
+}
+
+export const SearchBar: React.FC<SearchBarI> = ({ atom, sourceAtom }) => {
+  const setAtom = useSetAtom(atom);
+  const source = useAtomValue(sourceAtom);
+
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const search = event.target.value.toLocaleLowerCase();
+
+    setAtom(() => {
+      return source.filter((item) => item.name.toLocaleLowerCase().includes(search));
+    });
+  };
+
+  return <input type="search" className="form-group-input" placeholder="Search" onChange={changeHandler} />;
+};
+export default SearchBar;
