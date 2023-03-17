58dfaf1ffa1d4b8ccdd5c83f0e9dd40f09ec743b
ox7f <ox7f@protonmail.com>
2023-03-17 16:06:48 +0100

    fix: 🐛 removed unnecessary inputs

--- packages/frontend/src/atoms/database/agents.ts
+++ packages/frontend/src/atoms/database/agents.ts
@@ -8,8 +8,8 @@ Agents.Agents.forEach((agent) => {
   initialAgentData[agent.name] = {
     ...agent.stats,
     name: agent.name,
-    projectile_number: agent.stats.projectile_number ?? 1,
-    cast_time: agent.stats.cast_time ?? 0
+    // projectile_number: agent.stats.projectile_number ?? 1,
+    // cast_time: agent.stats.cast_time ?? 0
   };
 });
 
