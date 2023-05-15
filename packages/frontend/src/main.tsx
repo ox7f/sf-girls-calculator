86f1dfe54bc8cadcc9967e05ac5eb911e25d752f
ox7f <ox7f@protonmail.com>
2023-05-15 22:28:47 +0200

    feat: 🎸 vercel analytics

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -5,9 +5,12 @@ import { Provider } from 'jotai';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
 
 import { App } from './App';
 import { Calculator, ErrorBoundary, Home, Teamfinder } from './pages';
+
+inject();
 
 const router = createBrowserRouter([
   {
